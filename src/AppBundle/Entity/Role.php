fbb81dc20a730408a57f566fca30269bf149c029
Luis Ramón López <lrlopez@gmail.com>
2018-11-28 10:44:13 +0100

    Añadido rol de coordinador de FP dual

--- src/AppBundle/Entity/Role.php
+++ src/AppBundle/Entity/Role.php
@@ -28,8 +28,9 @@ use Doctrine\ORM\Mapping as ORM;
 class Role
 {
     const ROLE_LOCAL_ADMIN = 'LOCAL_ADMIN';
+    const ROLE_WLT_MANAGER = 'WLT_MANAGER';
 
-    const ROLES = [self::ROLE_LOCAL_ADMIN];
+    const ROLES = [self::ROLE_LOCAL_ADMIN, self::ROLE_WLT_MANAGER];
 
     /**
      * @ORM\Id
