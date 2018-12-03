87b4a145fffee02382714d7b19f3066e6b5b177a
Luis Ramón López <lrlopez@gmail.com>
2018-12-03 10:59:12 +0100

    Creadas entidades para gestionar las actividades
    Activity, ActivityRealization

---
+++ src/AppBundle/Entity/WLT/Activity.php
@@ -0,0 +1,176 @@
+<?php
+/*
+  Copyright (C) 2018: Luis Ramón López López
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see [http://www.gnu.org/licenses/].
+*/
+
+namespace AppBundle\Entity\WLT;
+
+use AppBundle\Entity\Edu\Competency;
+use AppBundle\Entity\Edu\Subject;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="wlt_activity")
+ */
+class Activity
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Edu\Subject")
+     * @ORM\JoinColumn(nullable=false)
+     * @var Subject
+     */
+    private $subject;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="text")
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="text")
+     * @var string
+     */
+    private $priorLearning;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Edu\Competency")
+     * @ORM\JoinTable(name="wlt_activity_competency")
+     * @ORM\OrderBy({"code": "ASC"})
+     * @var Competency[]
+     */
+    private $competencies;
+
+    public function __construct()
+    {
+        $this->competencies = new ArrayCollection();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Subject
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param Subject $subject
+     * @return Activity
+     */
+    public function setSubject(Subject $subject)
+    {
+        $this->subject = $subject;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     * @return Activity
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     * @return Activity
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPriorLearning()
+    {
+        return $this->priorLearning;
+    }
+
+    /**
+     * @param string $priorLearning
+     * @return Activity
+     */
+    public function setPriorLearning($priorLearning)
+    {
+        $this->priorLearning = $priorLearning;
+        return $this;
+    }
+
+    /**
+     * @return Competency[]|Collection
+     */
+    public function getCompetencies()
+    {
+        return $this->competencies;
+    }
+
+    /**
+     * @param Competency[] $competencies
+     * @return Activity
+     */
+    public function setCompetencies($competencies)
+    {
+        $this->competencies = $competencies;
+        return $this;
+    }
+}
---
+++ src/AppBundle/Entity/WLT/ActivityRealization.php
@@ -0,0 +1,141 @@
+<?php
+/*
+  Copyright (C) 2018: Luis Ramón López López
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see [http://www.gnu.org/licenses/].
+*/
+
+namespace AppBundle\Entity\WLT;
+
+use AppBundle\Entity\Edu\LearningOutcome;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="wlt_activity_realization")
+ */
+class ActivityRealization
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Activity")
+     * @ORM\JoinColumn(nullable=false)
+     * @var Activity
+     */
+    private $activity;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="text")
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Edu\LearningOutcome")
+     * @ORM\JoinTable(name="wlt_activity_realization_learning_outcome")
+     * @var LearningOutcome[]
+     */
+    private $learningOutcomes;
+
+    public function __construct()
+    {
+        $this->learningOutcomes = new ArrayCollection();
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Activity
+     */
+    public function getActivity()
+    {
+        return $this->activity;
+    }
+
+    /**
+     * @param Activity $activity
+     */
+    public function setActivity(Activity $activity)
+    {
+        $this->activity = $activity;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return LearningOutcome[]
+     */
+    public function getLearningOutcomes()
+    {
+        return $this->learningOutcomes;
+    }
+
+    /**
+     * @param LearningOutcome[] $learningOutcomes
+     */
+    public function setLearningOutcomes($learningOutcomes)
+    {
+        $this->learningOutcomes = $learningOutcomes;
+    }
+}
