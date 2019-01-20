69ea31f134139f1143a3364f2a90d8eaafb09b30
Luis Ramón López <lrlopez@gmail.com>
2019-01-20 22:28:47 +0100

    chore: Actualizado controlador de seguridad con DI

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -20,7 +20,10 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Organization;
 use AppBundle\Entity\User;
+use AppBundle\Form\Type\NewPasswordType;
+use AppBundle\Form\Type\PasswordResetType;
 use AppBundle\Repository\OrganizationRepository;
+use AppBundle\Repository\UserRepository;
 use AppBundle\Service\MailerService;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -29,16 +32,17 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class SecurityController extends Controller
 {
     /**
      * @Route("/entrar", name="login", methods={"GET"})
      */
-    public function loginAction()
+    public function loginAction(AuthenticationUtils $authenticationUtils)
     {
-        $authenticationUtils = $this->get('security.authentication_utils');
-
         // obtener el error de entrada, si existe alguno
         $error = $authenticationUtils->getLastAuthenticationError();
 
@@ -66,14 +70,19 @@ class SecurityController extends Controller
     /**
      * @Route("/restablecer", name="login_password_reset", methods={"GET", "POST"})
      */
-    public function passwordResetRequestAction(Request $request, MailerService $mailerService)
-    {
+    public function passwordResetRequestAction(
+        Request $request,
+        MailerService $mailerService,
+        Session $session,
+        TranslatorInterface $translator,
+        UserRepository $userRepository
+    ) {
 
         $data = [
             'email' => ''
         ];
 
-        $form = $this->createForm('AppBundle\Form\Type\PasswordResetType', $data);
+        $form = $this->createForm(PasswordResetType::class, $data);
 
         $form->handleRequest($request);
 
@@ -83,7 +92,7 @@ class SecurityController extends Controller
 
         // ¿se ha enviado una dirección?
         if ($form->isSubmitted() && $form->isValid()) {
-            $error = $this->passwordResetRequest($email, $mailerService);
+            $error = $this->passwordResetRequest($email, $mailerService, $translator, $userRepository);
 
             if (!is_string($error)) {
                 return $error;
@@ -93,7 +102,7 @@ class SecurityController extends Controller
         return $this->render(
             'security/login_password_reset.html.twig',
             [
-                'last_username' => $this->get('session')->get('_security.last_username', ''),
+                'last_username' => $session->get('_security.last_username', ''),
                 'form' => $form->createView(),
                 'error' => $error
             ]
@@ -103,18 +112,23 @@ class SecurityController extends Controller
     /**
      * @Route("/restablecer/correo/{userId}/{token}", name="email_reset_do", methods={"GET", "POST"})
      */
-    public function emailResetAction(Request $request, $userId, $token)
-    {
+    public function emailResetAction(
+        Request $request,
+        UserRepository $userRepository,
+        TranslatorInterface $translator,
+        $userId,
+        $token
+    ) {
         /**
          * @var User
          */
-        $user = $this->getDoctrine()->getManager()->getRepository('AppBundle:User')->findOneBy([
+        $user = $userRepository->findOneBy([
             'id' => $userId,
             'token' => $token
         ]);
 
         if (null === $user || $user->getTokenType() === 'password' || $user->getTokenExpiration() < new \DateTime()) {
-            $this->addFlash('error', $this->get('translator')->trans('form.change_email.notvalid', [], 'security'));
+            $this->addFlash('error', $translator->trans('form.change_email.notvalid', [], 'security'));
             return $this->redirectToRoute('login');
         }
 
@@ -131,11 +145,11 @@ class SecurityController extends Controller
                 // indicar que los cambios se han realizado con éxito y volver a la página de inicio
                 $this->addFlash(
                     'success',
-                    $this->get('translator')->trans('form.change_email.message', [], 'security')
+                    $translator->trans('form.change_email.message', [], 'security')
                 );
             } catch (\Exception $e) {
                 // indicar que no se ha podido cambiar
-                $this->addFlash('error', $this->get('translator')->trans('form.change_email.error', [], 'security'));
+                $this->addFlash('error', $translator->trans('form.change_email.error', [], 'security'));
             }
             return new RedirectResponse(
                 $this->generateUrl('frontpage')
@@ -153,19 +167,25 @@ class SecurityController extends Controller
     /**
      * @Route("/restablecer/{userId}/{token}", name="login_password_reset_do", methods={"GET", "POST"})
      */
-    public function passwordResetAction(Request $request, $userId, $token)
-    {
+    public function passwordResetAction(
+        Request $request,
+        UserRepository $userRepository,
+        PasswordEncoderInterface $passwordEncoder,
+        TranslatorInterface $translator,
+        $userId,
+        $token
+    ) {
         /**
          * @var User
          */
-        $user = $this->getDoctrine()->getManager()->getRepository('AppBundle:User')->findOneBy([
+        $user = $userRepository->findOneBy([
             'id' => $userId,
             'token' => $token,
             'tokenType' => 'password'
         ]);
 
         if (null === $user || ($user->getTokenExpiration() < new \DateTime())) {
-            $this->addFlash('error', $this->get('translator')->trans('form.reset.notvalid', [], 'security'));
+            $this->addFlash('error', $translator->trans('form.reset.notvalid', [], 'security'));
             return $this->redirectToRoute('login');
         }
 
@@ -174,15 +194,14 @@ class SecurityController extends Controller
             'repeat' => ''
         ];
 
-        $form = $this->createForm('AppBundle\Form\Type\NewPasswordType', $data);
+        $form = $this->createForm(NewPasswordType::class, $data);
 
         $form->handleRequest($request);
 
         $error = '';
         if ($form->isSubmitted() && $form->isValid()) {
             //codificar la nueva contraseña y asignarla al usuario
-            $password = $this->get('security.password_encoder')
-                ->encodePassword($user, $form->get('newPassword')->get('first')->getData());
+            $password = $passwordEncoder->encodePassword($user, $form->get('newPassword')->get('first')->getData());
 
             $user
                 ->setPassword($password)
@@ -193,7 +212,7 @@ class SecurityController extends Controller
             $this->getDoctrine()->getManager()->flush();
 
             // indicar que los cambios se han realizado con éxito y volver a la página de inicio
-            $message = $this->get('translator')->trans('form.reset.message', [], 'security');
+            $message = $translator->trans('form.reset.message', [], 'security');
             $this->addFlash('success', $message);
             return new RedirectResponse(
                 $this->generateUrl('frontpage')
@@ -213,20 +232,19 @@ class SecurityController extends Controller
     /**
      * @Route("/organizacion", name="login_organization", methods={"GET", "POST"})
      */
-    public function organizationAction(Request $request)
-    {
+    public function organizationAction(
+        Request $request,
+        Session $session,
+        OrganizationRepository $organizationRepository
+    ) {
         // si no hay usuario activo, volver
         if (null === $this->getUser()) {
             return $this->redirectToRoute('login');
         }
 
-        /** @var Session $session */
-        $session = $this->get('session');
-
         $data = ['organization' => $this->getUser()->getDefaultOrganization()];
 
-        $count = $this->getDoctrine()->getManager()->getRepository('AppBundle:Organization')->
-            countOrganizationsByUser($this->getUser(), new \DateTime());
+        $count = $$organizationRepository->countOrganizationsByUser($this->getUser(), new \DateTime());
 
         $form = $this->createFormBuilder($data)
             ->add('organization', EntityType::class, [
@@ -265,17 +283,20 @@ class SecurityController extends Controller
      * @param $email
      * @return string|RedirectResponse
      */
-    private function passwordResetRequest($email, MailerService $mailerService)
-    {
+    private function passwordResetRequest(
+        $email,
+        MailerService $mailerService,
+        TranslatorInterface $translator,
+        UserRepository $userRepository
+    ) {
         /** @var User $user */
         // comprobar que está asociada a un usuario
-        $user = $this->getDoctrine()->getManager()->getRepository('AppBundle:User')->
-            findOneBy(['emailAddress' => $email]);
+        $user = $userRepository->findOneBy(['emailAddress' => $email]);
 
         $error = '';
 
         if (null === $user) {
-            $error = $this->get('translator')->trans('form.reset.notfound', [], 'security');
+            $error = $translator->trans('form.reset.notfound', [], 'security');
         } else {
             // almacenar como último correo electrónico el indicado
             $this->get('session')->set('_security.last_username', $user->getEmailAddress());
@@ -284,12 +305,12 @@ class SecurityController extends Controller
             $expire = (int) $this->getParameter('password_reset.expire');
 
             if ($this->getParameter('external.enabled') && $user->getExternalCheck()) {
-                $this->addFlash('error', $this->get('translator')->
+                $this->addFlash('error', $translator->
                     trans('form.reset.external_login.error', [], 'security'));
             } else {
                 // comprobar que no se ha generado un token hace poco
                 if ($user->getToken() && $user->getTokenExpiration() > new \DateTime()) {
-                    $error = $this->get('translator')->trans('form.reset.wait', ['%expiry%' => $expire], 'security');
+                    $error = $translator->trans('form.reset.wait', ['%expiry%' => $expire], 'security');
                 } else {
                     // generar un nuevo token
                     $token = bin2hex(random_bytes(16));
@@ -318,14 +339,14 @@ class SecurityController extends Controller
                         ],
                         'security'
                     )) {
-                        $this->addFlash('error', $this->get('translator')->trans('form.reset.error', [], 'security'));
+                        $this->addFlash('error', $translator->trans('form.reset.error', [], 'security'));
                     } else {
                         // guardar token
                         $this->get('doctrine')->getManager()->flush();
 
                         $this->addFlash(
                             'success',
-                            $this->get('translator')->trans('form.reset.sent', ['%email%' => $email], 'security')
+                            $translator->trans('form.reset.sent', ['%email%' => $email], 'security')
                         );
                         return $this->redirectToRoute('login');
                     }
