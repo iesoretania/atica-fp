041db9c14926775db9a9898278299d0b21cdfa37
Luis Ramón López López <lrlopez@gmail.com>
2022-04-25 22:49:02 +0200

    fix: Corregido script no actualizado en el formulario de contacto con empresas

--- assets/js/wlt/contact.js
+++ assets/js/wlt/contact.js
@@ -5,8 +5,8 @@ $(function ()
         var form = $(this).closest('form');
         var data = {};
 
-        projects = $("#visit_projects");
-        var studentEnrollments = $("#visit_studentEnrollments");
+        projects = $("#contact_projects");
+        var studentEnrollments = $("#contact_studentEnrollments");
 
         data[date.attr('name')] = date.val();
         data[time.attr('name')] = time.val();
@@ -14,35 +14,35 @@ $(function ()
         data[workcenter.attr('name')] = workcenter.val();
 
         var next = projects.next();
-        $('#visit_projects').replaceWith('<div id="visit_projects"><span class="text-info"><i class="fas fa-circle-notch fa-spin fa-3x fa-fw"></i></span></div>');
+        $('#contact_projects').replaceWith('<div id="contact_projects"><span class="text-info"><i class="fas fa-circle-notch fa-spin fa-3x fa-fw"></i></span></div>');
         next.remove();
         next = studentEnrollments.next();
-        $('#visit_studentEnrollments').replaceWith('<div id="visit_studentEnrollments"><span class="text-info"><i class="fas fa-circle-notch fa-spin fa-3x fa-fw"></i></span></div>');
+        $('#contact_studentEnrollments').replaceWith('<div id="contact_studentEnrollments"><span class="text-info"><i class="fas fa-circle-notch fa-spin fa-3x fa-fw"></i></span></div>');
         next.remove();
         $.ajax({
             url: form.attr('data-ajax'),
             type: form.attr('method'),
             data: data,
             success: function (html) {
-                $('#visit_projects').replaceWith(
-                    $(html).find('#visit_projects')
+                $('#contact_projects').replaceWith(
+                    $(html).find('#contact_projects')
                 );
-                $('select#visit_projects').select2({
+                $('select#contact_projects').select2({
                     theme: "bootstrap",
                     language: 'es'
                 });
-                $('#visit_studentEnrollments').replaceWith(
-                    $(html).find('#visit_studentEnrollments')
+                $('#contact_studentEnrollments').replaceWith(
+                    $(html).find('#contact_studentEnrollments')
                 );
-                $('select#visit_studentEnrollments').select2({
+                $('select#contact_studentEnrollments').select2({
                     theme: "bootstrap",
                     language: 'es'
                 });
-                $('#visit_projects input').change(projectsChange);
+                $('#contact_projects input').change(projectsChange);
             },
             error: function () {
-                $('#visit_projects').replaceWith('<div id="visit_projects"><span class="text-danger"><i class="fas fa-times-circle fa-3x"></i></span></div>');
-                $('#visit_studentEnrollments').replaceWith('<div id="visit_studentEnrollments"><span class="text-danger"><i class="fas fa-times-circle fa-3x"></i></span></div>');
+                $('#contact_projects').replaceWith('<div id="contact_projects"><span class="text-danger"><i class="fas fa-times-circle fa-3x"></i></span></div>');
+                $('#contact_studentEnrollments').replaceWith('<div id="contact_studentEnrollments"><span class="text-danger"><i class="fas fa-times-circle fa-3x"></i></span></div>');
             }
         });
     }
@@ -52,47 +52,47 @@ $(function ()
         var form = $(this).closest('form');
         var data = {};
 
-        var studentEnrollments = $("#visit_studentEnrollments");
+        var studentEnrollments = $("#contact_studentEnrollments");
 
         data[date.attr('name')] = date.val();
         data[time.attr('name')] = time.val();
         data[teacher.attr('name')] = teacher.val();
         data[workcenter.attr('name')] = workcenter.val();
         var checked = [];
-        $("#visit_projects input:checked").each(function () {
+        $("#contact_projects input:checked").each(function () {
             checked.push($(this).val());
         });
         data['visit[projects][]'] = checked;
 
         var next = studentEnrollments.next();
-        $('#visit_studentEnrollments').replaceWith('<div id="visit_studentEnrollments"><span class="text-info"><i class="fas fa-circle-notch fa-spin fa-3x fa-fw"></i></span></div>');
+        $('#contact_studentEnrollments').replaceWith('<div id="contact_studentEnrollments"><span class="text-info"><i class="fas fa-circle-notch fa-spin fa-3x fa-fw"></i></span></div>');
         next.remove();
         $.ajax({
             url: form.attr('data-ajax'),
             type: form.attr('method'),
             data: data,
             success: function (html) {
-                $('#visit_studentEnrollments').replaceWith(
-                    $(html).find('#visit_studentEnrollments')
+                $('#contact_studentEnrollments').replaceWith(
+                    $(html).find('#contact_studentEnrollments')
                 );
-                $('select#visit_studentEnrollments').select2({
+                $('select#contact_studentEnrollments').select2({
                     theme: "bootstrap",
                     language: 'es'
                 });
             },
             error: function () {
-                $('#visit_studentEnrollments').replaceWith('<div id="visit_studentEnrollments"><span class="text-danger"><i class="fas fa-times-circle fa-3x"></i></span></div>');
+                $('#contact_studentEnrollments').replaceWith('<div id="contact_studentEnrollments"><span class="text-danger"><i class="fas fa-times-circle fa-3x"></i></span></div>');
             }
         });
     }
 
-    var teacher = $("#visit_teacher");
-    var workcenter = $("#visit_workcenter");
-    var projects = $("#visit_projects");
-    var date = $("#visit_dateTime_date");
-    var time = $("#visit_dateTime_time");
+    var teacher = $("#contact_teacher");
+    var workcenter = $("#contact_workcenter");
+    var projects = $("#contact_projects");
+    var date = $("#contact_dateTime_date");
+    var time = $("#contact_dateTime_time");
 
     date.change(workcenterChange);
     workcenter.change(workcenterChange);
-    $('#visit_projects input').change(projectsChange);
+    $('#contact_projects input').change(projectsChange);
 });
